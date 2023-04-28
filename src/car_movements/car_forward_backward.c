162f09633aeebb1d7bc2a048a8d9a9765d415dbe
Thomaltarix <thomas.boue@epitech.eu>
2023-04-28 19:56:32 +0200

    :bug: Add fflush(stdout) to avoid printf cache issues

--- src/car_movements/car_forward_backward.c
+++ src/car_movements/car_forward_backward.c
@@ -12,25 +12,31 @@
 void go_forward(float speed)
 {
     if (speed > 1) {
-        printf("CAR_FORWARD:%.1f\n", 1);
+        printf("CAR_FORWARD:%.1f\n", 1.0);
+        fflush(stdout);
         return;
     }
     if (speed < 0) {
-        printf("CAR_FORWARD:%.1f\n", 0);
+        printf("CAR_FORWARD:%.1f\n", 0.0);
+        fflush(stdout);
         return;
     }
-    printf("CAR_FORWARD:%.1f\n", 1);
+    printf("CAR_FORWARD:%.1f\n", speed);
+    fflush(stdout);
 }
 
 void go_backward(float speed)
 {
     if (speed > 1) {
-        printf("CAR_BACKWARDS:%.1f\n", 1);
+        printf("CAR_BACKWARDS:%.1f\n", 1.0);
+        fflush(stdout);
         return;
     }
     if (speed < 0) {
-        printf("CAR_BACKWARDS:%.1f\n", 0);
+        printf("CAR_BACKWARDS:%.1f\n", 0.0);
+        fflush(stdout);
         return;
     }
-    printf("CAR_BACKWARDS:%.1f\n", 1);
+    printf("CAR_BACKWARDS:%.1f\n", speed);
+    fflush(stdout);
 }
