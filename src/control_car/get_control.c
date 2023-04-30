75f056c879c251f1529a5fd4f23b39dd2cbf9366
Marius-P1 <marius.pain@epitech.eu>
2023-04-30 22:37:12 +0200

    :rotating_light: Fixed linter but better

--- src/control_car/get_control.c
+++ src/control_car/get_control.c
@@ -49,23 +49,23 @@ bool *error)
 {
     if (distance >= 100) {
         if (global->old_speed == 0.05)
-            return(0.55);
+            return (0.55);
         printf("CAR_FORWARD:0.05\n");
         fflush(stdout);
         get_no_error_infos_bool(error, finish);
         global->old_speed = 0.05;
-        return(0.55);
+        return (0.55);
     }
     if (distance < 100) {
         if (global->old_speed == 0.05)
-            return(0.7);
+            return (0.7);
         printf("CAR_FORWARD:0.05\n");
         fflush(stdout);
         get_no_error_infos_bool(error, finish);
         global->old_speed = 0.05;
-        return(0.7);
+        return (0.7);
     }
-    return(1.0);
+    return (1.0);
 }
 
 float set_car_speed2(float distance, nfs_returns_t *global, bool *finish,
@@ -73,21 +73,21 @@ bool *error)
 {
     if (distance >= 500) {
         if (global->old_speed == 0.25)
-            return(0.15);
+            return (0.15);
         printf("CAR_FORWARD:0.25\n");
         fflush(stdout);
         get_no_error_infos_bool(error, finish);
         global->old_speed = 0.25;
-        return(0.15);
+        return (0.15);
     }
     if (distance >= 200) {
         if (global->old_speed == 0.15)
-            return(0.3);
+            return (0.3);
         printf("CAR_FORWARD:0.15\n");
         fflush(stdout);
         get_no_error_infos_bool(error, finish);
         global->old_speed = 0.15;
-        return(0.3);
+        return (0.3);
     }
     return set_car_speed3(distance, global, finish, error);
 }
@@ -97,21 +97,21 @@ bool *finish, bool *error)
 {
     if (distance >= 2000) {
         if (global->old_speed == 0.55)
-            return(0.01);
+            return (0.01);
         printf("CAR_FORWARD:0.55\n");
         fflush(stdout);
         get_no_error_infos_bool(error, finish);
         global->old_speed = 0.55;
-        return(0.01);
+        return (0.01);
     }
     if (distance >= 1000) {
         if (global->old_speed == 0.35)
-            return(0.01);
+            return (0.01);
         printf("CAR_FORWARD:0.35\n");
         fflush(stdout);
         get_no_error_infos_bool(error, finish);
         global->old_speed = 0.35;
-        return(0.01);
+        return (0.01);
     }
     return set_car_speed2(distance, global, finish, error);
 }
