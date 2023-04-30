303932bdd71bfd89de8223c04099cd202dd2b7d7
Marius-P1 <marius.pain@epitech.eu>
2023-04-30 22:29:57 +0200

    :zap: Updated car movements function

--- src/car_movements/car_wheels_movements.c
+++ src/car_movements/car_wheels_movements.c
@@ -10,32 +10,32 @@
 
 void turn_left(float angle)
 {
-    if (angle > 1) {
-        printf("WHEELS_DIR:-%.1f\n", 1.0);
+    if (angle > 1.0000) {
+        printf("WHEELS_DIR:-%.2f\n", 1.00);
         fflush(stdout);
         return;
     }
-    if (angle < 0) {
-        printf("WHEELS_DIR:-%.1f\n", 0.0);
+    if (angle < 0.0000) {
+        printf("WHEELS_DIR:-%.2f\n", 0.00);
         fflush(stdout);
         return;
     }
-    printf("WHEELS_DIR:-%.1f\n", angle);
+    printf("WHEELS_DIR:-%.2f\n", angle);
     fflush(stdout);
 }
 
 void turn_right(float angle)
 {
-    if (angle > 1) {
-        printf("WHEELS_DIR:%.1f\n", 1.0);
+    if (angle > 1.0000) {
+        printf("WHEELS_DIR:%.2f\n", 1.00);
         fflush(stdout);
         return;
     }
-    if (angle < 0) {
-        printf("WHEELS_DIR:%.1f\n", 0.0);
+    if (angle < 0.0000) {
+        printf("WHEELS_DIR:%.2f\n", 0.00);
         fflush(stdout);
         return;
     }
-    printf("WHEELS_DIR:%.1f\n", angle);
+    printf("WHEELS_DIR:%.2f\n", angle);
     fflush(stdout);
 }
